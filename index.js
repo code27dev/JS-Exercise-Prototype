6d95df62b44860df37db1a718557e1cdc595aec8
code27dev <55630787+code27dev@users.noreply.github.com>
2020-04-30 17:22:25 -0700

    finished

--- index.js
+++ index.js
@@ -39,8 +39,21 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
+  Person.prototype.eat = function(food) {
+    if (this.stomach.length < 10) {
+      this.stomach.push(food);
+    }
+  };
+  Person.prototype.poop = function() {
+    this.stomach = [];
+  };
+  Person.prototype.toString = function() {
+    return `${this.name}, ${this.age}`;
+  };
 }
 
 /*
@@ -57,8 +70,25 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model, mpg) {
+  this.model = model;
+  this.milesPerGallon = mpg;
+  this.tank = 0;
+  this.odometer = 0;
+  Car.prototype.fill = function(gallons) {
+    this.tank += gallons;
+  };
+  Car.prototype.drive = function(distance) {
+    this.odometer = this.odometer + distance;
+    let gallonsused = distance / this.milesPerGallon;
+    this.tank -= gallonsused;
+    if (this.tank < 0) {
+      let newdistance = Math.floor(this.tank / this.milesPerGallon) * -1;
+      this.odometer -= newdistance;
+      this.tank = 0;
+      return `I ran out of fuel at ${distance - newdistance}`;
+    }
+  }
 }
 
 /*
@@ -68,8 +98,13 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
-
+function Baby(name, age, favoriteToy) {
+    Person.call(this, name, age);
+    Baby.prototype = Object.create(Person.prototype);
+    this.favoriteToy = favoriteToy;
+    Baby.prototype.play = function() {
+      return `Playing with ${this.favoriteToy}`;
+    };
 }
 
 /* 
